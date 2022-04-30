f4e8b0d8fd7cbcf5d5a93d035a2f3d6f6a416c8d
arifibnenam <arifibnenam@gmail.com>
2022-04-30 11:58:38 +0600

    server running and get default route

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
-
+const port = process.env.PORT || 5000;
 // midleware 
 app.use(cors());
 app.use(express.json())
-app.use(dotenv());
+
+const uri = "mongodb+srv://warehouse_admin:5KhBGbwA7XInVlpv@cluster0.tdnyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+app.get('/', (req, res) => {
+    res.send('Running warehouse management server');
+})
+
+async function run() {
+    try {
+        await client.connect();
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir());
+app.listen(port, () => {
+    console.log('server is running on port', port);
+})
